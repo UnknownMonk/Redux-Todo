2d540f927d46d8405546bb8c9a652a65b72a7457
UnknownMonk <smodcartoon@gmail.com>
2019-03-05 14:18:40 -0500

    added reducers to todo and form

---
+++ todo/src/reducers/reducer.js
@@ -0,0 +1,30 @@
+import { ADD_TODO, COMPLETE_TODO, DELETE_TODO } from '../actions/actions';
+
+const initialState = [
+  {
+    task: 'DONT PANIC',
+    completed: false,
+    id: Date.now()
+  }
+];
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_TODO:
+      return state.concat(action.payload);
+    case COMPLETE_TODO:
+      return state.map(todo => {
+        if (todo.id === action.payload) {
+          return { ...todo, completed: !todo.completed };
+        } else {
+          return todo;
+        }
+      });
+    case DELETE_TODO:
+      return state.filter(todo => {
+        return todo.id !== action.payload;
+      });
+    default:
+      return state;
+  }
+};
